d96ec9764bf2cf61c783f6b62eee86afc938f219
Aigars Sinevics <aigarzs@gmail.com>
2024-05-24 13:53:07 +0300

    Initial page added

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
@@ -10,4 +10,4 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 @app.get("/")
 def get_index():
-    return "Hello Site Analyzer!"
+    return render_template("index.html")
