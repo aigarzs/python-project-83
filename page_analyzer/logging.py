92983398fb1ea0a5498e9332c88392df8235214e
Aigars Sinevics <aigarzs@gmail.com>
2024-07-04 21:19:53 +0300

    Corrected line break after binary operator

--- page_analyzer/logging.py
+++ page_analyzer/logging.py
@@ -1,8 +1,8 @@
 def debug(func):
     def inner(*args, **kwargs):
-        print(func.__name__ +
-              " *args: " + str(*args) +
-              ", **kwargs: " + str(**kwargs))
+        print(func.__name__
+              + " *args: " + str(*args)
+              + ", **kwargs: " + str(**kwargs))
         result = func(*args, **kwargs)
         print(func.__name__ + " result: " + str(result))
         return result
